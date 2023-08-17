eb2562abc2fab4eacced98841e62a56eeee8180b
Anselm Schüler <mail@anselmschueler.com>
2023-08-17 19:21:59 +0200

    Fix a few things & use ? & try

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 #![feature(array_try_map)]
 #![feature(array_try_from_fn)]
 #![feature(exact_size_is_empty)]
+#![feature(try_blocks)]
 
 use std::{num::ParseIntError, process::ExitCode};
 
 enum Error {
     TooFewArguments,
     ParseError(usize, ParseIntError),
-}
-
-impl From<(usize, ParseIntError)> for Error {
-    fn from((i, err): (usize, ParseIntError)) -> Self {
-        Error::ParseError(i, err)
-    }
 }
 
 fn report_usage(pname: Option<String>) -> ExitCode {
@@ -30,22 +25,26 @@ fn report_parse_failure(arg: &str, err: ParseIntError) -> ExitCode {
 fn main() -> ExitCode {
     let mut args = std::env::args();
     let pname = args.next();
-    let parsed =
-        std::array::try_from_fn(|i| args.next().map(|s| (i, s)).ok_or(Error::TooFewArguments))
-            .and_then(|args| args.try_map(|(i, s)| s.parse().map_err(|err| (i, err).into())));
+    // let parsed =
+    //     std::array::try_from_fn(|i| args.next().map(|s| (i, s)).ok_or(Error::TooFewArguments))
+    //         .and_then(|args| args.try_map(|(i, s)| s.parse().map_err(|err| (i, err).into())));
+    let args_arr: Result<[(usize, String); 3], Error> =
+        std::array::try_from_fn(|i| Ok((i, args.next().ok_or(Error::TooFewArguments)?)));
+    let parsed: Result<[usize; 3], Error> =
+        try { args_arr?.try_map(|(i, s)| s.parse().map_err(|err| Error::ParseError(i, err)))? };
     if !args.is_empty() {
         return report_usage(pname);
     }
     let (x, y, z) = match parsed {
         Ok([x, y, z]) => (x, y, z),
         Err(Error::TooFewArguments) => return report_usage(pname),
-        Err(Error::ParseError(1, err)) => return report_parse_failure("x", err),
-        Err(Error::ParseError(2, err)) => return report_parse_failure("y", err),
-        Err(Error::ParseError(3, err)) => return report_parse_failure("z", err),
+        Err(Error::ParseError(0, err)) => return report_parse_failure("x", err),
+        Err(Error::ParseError(1, err)) => return report_parse_failure("y", err),
+        Err(Error::ParseError(2, err)) => return report_parse_failure("z", err),
         Err(_) => unreachable!(),
     };
     let mut counter = 0;
-    let mut arr: Vec<Vec<Vec<u64>>> = Vec::with_capacity(x);
+    let mut arr: Vec<Vec<Vec<u128>>> = Vec::with_capacity(x);
     counter += 1;
     for i in 0..x {
         arr.push(Vec::with_capacity(y));
@@ -54,7 +53,11 @@ fn main() -> ExitCode {
             arr[i].push(Vec::with_capacity(z));
             counter += 1;
             for k in 0..z {
-                arr[i][j].push(2u64.pow(i as u32) + 3u64.pow(j as u32) + 4u64.pow(k as u32))
+                arr[i][j].push(
+                    (2u64.pow(i as u32) as u128)
+                        * (3u64.pow(j as u32) as u128)
+                        * (4u64.pow(k as u32) as u128),
+                )
             }
         }
     }
